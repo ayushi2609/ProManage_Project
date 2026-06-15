7d9b0b0de9682f51b0de6b35debaf042be8e4ad9
“Ayushi <ayushibhatia4409@gmail.com>
2026-06-15 16:47:52 +0530

    backend complete

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+// import { serve } from "inngest/express";
+// import { inngest, functions } from "./inngest/index.js"
 import workspaceRouter from './routes/workspaceRoutes.js';
 import { protect } from './middlewares/authMiddleware.js';
 import projectRouter from './routes/projectRoutes.js';
@@ -18,7 +18,7 @@ app.use(clerkMiddleware());
 
 app.get('/', (req, res)=> res.send('Server is live!'));
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+// app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Routes
 app.use("/api/workspaces", protect, workspaceRouter)
